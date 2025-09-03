c2ce97d6bb83b09b16e31883d3f3c95b070aad8a
miaoyiqaq <2584963786@qq.com>
2025-09-03 22:28:32 +0800

    J20250722homework

---
+++ practice/J20250723/src/Partition.java
@@ -0,0 +1,29 @@
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Partition {
+    
+    
+    public ListNode partition(ListNode pHead, int x) {
+        // write code here
+        ListNode curN = pHead.next;
+        ListNode cur = pHead;
+        ListNode end = null;
+        ListNode tmp = pHead;
+        while(tmp != null){//找到end节点
+            tmp = tmp.next;
+            end = tmp;
+        }
+        while(cur.next != null){
+            if(cur.val < x){
+                end.next = cur;
+
+            }
+        }
+}
