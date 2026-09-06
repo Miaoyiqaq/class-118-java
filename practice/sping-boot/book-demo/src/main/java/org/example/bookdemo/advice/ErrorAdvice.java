fd5f3a5a941405fbe30dcc10f4e230aa8b52f896
miaoyiqaq <2584963786@qq.com>
2026-09-06 18:27:15 +0800

    完善前后端接口约束

--- practice/sping-boot/book-demo/src/main/java/org/example/bookdemo/advice/ErrorAdvice.java
+++ practice/sping-boot/book-demo/src/main/java/org/example/bookdemo/advice/ErrorAdvice.java
@@ -5,6 +5,7 @@ import org.example.bookdemo.model.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @ControllerAdvice
@@ -17,16 +18,21 @@ public class ErrorAdvice {
     }
 
     @ExceptionHandler
-    public Result handler(ArithmeticException e){
-        return Result.fail(e.getMessage(), "发生算术异常");
+    public Result exceptionHandler (NoResourceFoundException e){
+        log.error("异常捕获：" + e.getMessage());
+        return Result.fail(e.getMessage(), "文件不存在:" + e.getResourcePath());
     }
-
-    @ExceptionHandler
-    public Result handler(ArrayIndexOutOfBoundsException e){
-        return Result.fail(e.getMessage(), "发生数组越界异常");
-    }
-    @ExceptionHandler
-    public Result handler(NullPointerException e){
-        return Result.fail(e.getMessage(), "发生空指针异常");
-    }
+//    @ExceptionHandler
+//    public Result handler(ArithmeticException e){
+//        return Result.fail(e.getMessage(), "发生算术异常");
+//    }
+//
+//    @ExceptionHandler
+//    public Result handler(ArrayIndexOutOfBoundsException e){
+//        return Result.fail(e.getMessage(), "发生数组越界异常");
+//    }
+//    @ExceptionHandler
+//    public Result handler(NullPointerException e){
+//        return Result.fail(e.getMessage(), "发生空指针异常");
+//    }
 }
--- practice/sping-boot/book-demo/src/main/java/org/example/bookdemo/config/AppConfig.java
+++ practice/sping-boot/book-demo/src/main/java/org/example/bookdemo/config/AppConfig.java
@@ -6,14 +6,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
+
+    private List<String> excludePathPatterns = List.of(
+            "/user/login",
+            "/test/**",
+            "/error",
+            "/**/*.html",
+            "/css/**",
+            "/js/**",
+            "/pic/**",
+            "favicon.ico"
+    );
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor).addPathPatterns("/**")
-                .excludePathPatterns("/user/login")
-                .excludePathPatterns("/test/**", "/error");
+                .excludePathPatterns(excludePathPatterns);
     }
 }
